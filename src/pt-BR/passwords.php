c916199ecdc5255341447e06e8b4df3fff0457cc
Ivens Pontes <ivenscardoso@hotmail.com>
2020-04-01 04:11:15 -0300

    Update passwords.php
    add throttled

--- src/pt-BR/passwords.php
+++ src/pt-BR/passwords.php
@@ -16,6 +16,7 @@ return [
     'password' => 'A senha e a confirmação devem combinar e possuir pelo menos seis caracteres.',
     'reset' => 'Sua senha foi redefinida!',
     'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
+    'throttled' => 'Aguarde antes de tentar novamente.',
     'token' => 'Este token de redefinição de senha é inválido.',
     'user' => "Não encontramos um usuário com esse endereço de e-mail.",
 
