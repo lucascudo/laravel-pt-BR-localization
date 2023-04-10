c7d645d8a9528d9f67553098b3f6c7012747c61a
Teixeira Rocha, Lucas <lucas.teixeira.rocha@oracle.com>
2023-04-10 12:34:00 +0100

    Update LaravelPtBRLocalizationServiceProvider.php

--- src/LaravelPtBRLocalizationServiceProvider.php
+++ src/LaravelPtBRLocalizationServiceProvider.php
@@ -14,10 +14,8 @@ class LaravelPtBRLocalizationServiceProvider extends ServiceProvider
     public function register()
     {
         $langPath = explode('.', $this->app->version())[0] > 8 ? '/lang' : '/resources/lang';
-        echo $langPath;
-        die($this->app->langPath());
         $this->publishes([
-            __DIR__ . '/pt-BR.json' =>  $langPath . '/pt-BR.json',
+            __DIR__ . '/pt-BR.json' => $langPath . '/pt-BR.json',
             __DIR__ . '/pt-BR' => $langPath . '/pt-BR',
         ], 'laravel-pt-br-localization');
     }
