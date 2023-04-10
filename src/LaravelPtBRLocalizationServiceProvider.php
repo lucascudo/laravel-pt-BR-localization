b4494045d31b6f1b280969d0804b2ec7a687031a
Teixeira Rocha, Lucas <lucas.teixeira.rocha@oracle.com>
2023-04-10 12:38:43 +0100

    GH Actions working!

--- src/LaravelPtBRLocalizationServiceProvider.php
+++ src/LaravelPtBRLocalizationServiceProvider.php
@@ -15,8 +15,7 @@ class LaravelPtBRLocalizationServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/pt_BR.json' => $this->app->langPath() . '/pt_BR.json',
-            __DIR__ . '/pt_BR' => $this->app->langPath() . '/pt_BR',
-            $this->app->langPath() . '/bla' => __DIR__ . 'bla'
+            __DIR__ . '/pt_BR' => $this->app->langPath() . '/pt_BR'
         ], 'laravel-pt-br-localization');
     }
 }
